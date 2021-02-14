dfe3702b9dc8d404cc0fd587c763be5fbc737bb7
pravin.deshmukh12@gmail.com <pravin.deshmukh12@gmail.com>
2021-02-14 15:14:06 +0530

    Add project files.

---
+++ Eureka.Client/Modules/HomeModule.cs
@@ -0,0 +1,20 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eureka.Nancy.API.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule() : base("Home")
+        {
+
+            Get("/", (parameters, token) =>
+            {
+                return Task.FromResult<dynamic>("Hello from some service!");
+            });
+        }
+    }
+}
---
+++ Eureka.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Common.Hosting;
+using System;
+using System.IO;
+
+namespace Eureka.Nancy.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                    .UseContentRoot(Directory.GetCurrentDirectory()) // Nancy Stuff
+                    .UseKestrel(o => o.AllowSynchronousIO = true) // Nancy Stuff
+                    .UseStartup<Startup>();
+                })
+            .ConfigureAppConfiguration((hostContext, configApp) =>
+            {
+                configApp.AddJsonFile("appsettings.json", optional: true);
+            })
+
+            .UseCloudHosting(50501) // For Local
+            ;
+    }
+}
